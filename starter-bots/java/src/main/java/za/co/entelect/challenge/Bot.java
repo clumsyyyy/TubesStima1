1495a355c38f515fe4267e359952684f7f8d9f20
clumsyyyy <owencwijaya@gmail.com>
2022-02-05 11:48:08 +0700

    add simple algo and dependencies

--- starter-bots/java/src/main/java/za/co/entelect/challenge/Bot.java
+++ starter-bots/java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -1,9 +1,9 @@
-package za.co.entelect.challenge;
-// FILE INI YANG HARUS DIUPDATE
+package za.co.entelect.challenge;               
+
 import za.co.entelect.challenge.command.*;
 import za.co.entelect.challenge.entities.*;
-import za.co.entelect.challenge.enums.Terrain;
 import za.co.entelect.challenge.enums.PowerUps;
+import za.co.entelect.challenge.enums.Terrain;
 
 import java.util.*;
 
@@ -20,20 +20,30 @@ public class Bot {
     private Car opponent;
     private Car myCar;
 
-    // inisialisasi commands
+    // inisialisasi commands sesuai urutan di game-rules.md
+
+    // nothing / accel / decel
+    private final static Command NOTHING = new DoNothingCommand();
     private final static Command ACCELERATE = new AccelerateCommand();
-    private final static Command LIZARD = new LizardCommand();
-    private final static Command OIL = new OilCommand();
-    private final static Command BOOST = new BoostCommand();
-    private final static Command EMP = new EmpCommand();
+    private final static Command DECELERATE = new DecelerateCommand();
+
+    // belok kiri / kanan
+    private final static Command TURN_LEFT = new ChangeLaneCommand(-1);
+    private final static Command TURN_RIGHT = new ChangeLaneCommand(1);
+
+    // power ups
+    // [IMPORTANT] instance tweet gapunya default constructor,
+    // jadi ngecallnya pas mau pake aja 
+    // TODO: algoritma buat nentuin tempat yang pas buat pake tweetnya
+
+    private final static Command USE_BOOST = new BoostCommand();
+    private final static Command USE_OIL = new OilCommand();
+    private final static Command USE_LIZARD = new LizardCommand();
+    private final static Command USE_EMP = new EmpCommand();
     private final static Command FIX = new FixCommand();
 
-    // inisialisasi belok kiri / kanan
-    private final static Command LEFT = new ChangeLaneCommand(-1);
-    private final static Command RIGHT = new ChangeLaneCommand(1);
-
     // ini bagian public
-    // inisialisasi instance nya 
+    // user-defined constructor
     // TODO: opponent gimana maksudnya?
     public Bot(Random random, GameState gameState) {
         this.random = random;
@@ -41,22 +51,68 @@ public class Bot {
         this.myCar = gameState.player;
         this.opponent = gameState.opponent;
 
-        directionList.add(LEFT);
-        directionList.add(RIGHT);
+        directionList.add(TURN_LEFT);
+        directionList.add(TURN_RIGHT);
     }
 
 
-    // ========== PUBLIC =========
+    // INI YANG DIKOTAK KATIK
     public Command run() {
-        List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block);
-        if (myCar.damage >= 5) {
-            return new FixCommand();
+
+        //inisialisasi list blocks yang bisa dilihat di depan mobil kita
+        //ket. p = player, o = opponent
+        List<Object> pBlocks = getBlocksInFront(myCar.position.lane, myCar.position.block);
+        //List<Object> oBlocks = getBlocksInFront(opponent.position.lane, opponent.position.block);
+        List<Object> pNextBlocks = pBlocks.subList(0,1);
+        //List<Object> oNextBlocks = oBlocks.subList(0,1);
+        if(myCar.damage == 5) {
+            return FIX;
         }
-        if (blocks.contains(Terrain.MUD)) {
-            int i = random.nextInt(directionList.size());
-            return new ChangeLaneCommand(directionList.get(i));
+        // kalau damage mobil >= 5, langsung baikin
+        // karena kalo damage >= 5, mobil langsung gabisa gerak
+
+        if (myCar.speed <= 3){
+            return ACCELERATE;
         }
-        return new AccelerateCommand();
+
+        if (myCar.damage >= 5){
+            return FIX;
+        }
+
+        // algoritma sederhana pengecekan apakah ada mud di depan / ada wall di depan
+        // .contains(ELMT) dipake untuk tau apakah di dalem list ada ELMT tersebut
+        if (pBlocks.contains(Terrain.MUD) || pNextBlocks.contains(Terrain.WALL)){
+            if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)){
+                return USE_LIZARD;
+            }
+            if (pNextBlocks.contains(Terrain.MUD) || pNextBlocks.contains(Terrain.WALL)){
+                if (myCar.position.lane == 1){          //kalau misalnya di lane 1, turn right biar ga minus
+                    return TURN_RIGHT;
+                } else if (myCar.position.lane == 4){   //kalau misalnya di lane 4, turn left biar ga minus
+                    return TURN_LEFT;
+                } else {                                //kalau misalnya ngga di situ, bebas
+                    return directionList.get(random.nextInt(directionList.size()));
+                }
+            }
+        }
+
+        // boost kalo nganggur
+        if (hasPowerUp(PowerUps.BOOST, myCar.powerups)){
+            return USE_BOOST;
+        }
+
+        // aggression, sementara ngambil dari ref bot, nanti tambahin tweet di sini
+        if (myCar.speed == maxSpeed) {
+            if (hasPowerUp(PowerUps.OIL, myCar.powerups)) {
+                return USE_OIL;
+            }
+            if (hasPowerUp(PowerUps.EMP, myCar.powerups)) {
+                return USE_EMP;
+            }
+        }
+        // kalo di depan ga ada masalah apa-apa
+        // g a s
+        return ACCELERATE;
     }
 
 
@@ -82,5 +138,14 @@ public class Bot {
         }
         return blocks;
     }
+    // fungsi untuk mengecek apakah powerup yang mau dipakai ada di list
 
+    private Boolean hasPowerUp(PowerUps powerUpToCheck, PowerUps[] available) {
+        for (PowerUps powerUp: available) {
+            if (powerUp.equals(powerUpToCheck)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
