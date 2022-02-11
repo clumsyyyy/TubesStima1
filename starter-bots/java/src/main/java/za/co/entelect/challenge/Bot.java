f4b2b1cbae2191aff205b8e4bb3112fb20cb0df6
clumsyyyy <owencwijaya@gmail.com>
2022-02-11 20:47:38 +0700

    help

--- starter-bots/java/src/main/java/za/co/entelect/challenge/Bot.java
+++ starter-bots/java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -62,14 +62,24 @@ public class Bot {
         //inisialisasi list blocks yang bisa dilihat di depan mobil kita
         //ket. p = player, o = opponent
         List <Object> pNextBlocks;
-        List<Object> pBlocks = getBlocksInFront(myCar.position.lane, myCar.position.block);
-        if (pBlocks.size() < min(myCar.speed, trackLength - myCar.position.block + 1)) {
-            pNextBlocks = pBlocks.subList(0, min(myCar.speed, trackLength - myCar.position.block + 1));
+        List <Object> currentLane = getBlocksInFront(myCar.position.lane, myCar.position.block, myCar.speed);
+        if (currentLane.size() < min(myCar.speed, trackLength - myCar.position.block + 1)) {
+            pNextBlocks = currentLane.subList(0, min(myCar.speed, trackLength - myCar.position.block + 1));
         } else {
-            pNextBlocks = pBlocks;
+            pNextBlocks = currentLane;
         }
+        List <Object> leftLane = getBlocksInFront(myCar.position.lane - 1, myCar.position.block, myCar.speed);
+        List <Object> rightLane = getBlocksInFront(myCar.position.lane + 1, myCar.position.block, myCar.speed);
+        List <Object> boostLane = getBlocksInFront(myCar.position.lane, myCar.position.block, 15);
+        List <Object> accelLane = getBlocksInFront(myCar.position.lane, myCar.position.block, nextSpeedState(myCar));
 
+        // implementasi algoritma kalau di depan lane kosong / tidak ada obstacle
 
+        if (Obstacles(currentLane) == 0){
+            if (isInSameLane(myCar, opponent)){
+
+            }
+        }
         // kalau damage mobil >= 5, langsung baikin
         // karena kalo damage >= 5, mobil langsung gabisa gerak
         if (myCar.damage >= 4){
@@ -79,7 +89,7 @@ public class Bot {
             return ACCELERATE;
         }
 
-        if (Obstacles(pBlocks) <= 1) {
+        if (Obstacles(currentLane) <= 1) {
             if (hasPowerUp(PowerUps.BOOST, myCar.powerups)){
                 return USE_BOOST;
             }
@@ -148,13 +158,13 @@ public class Bot {
      * Returns map of blocks and the objects in the for the current lanes, returns the amount of blocks that can be
      * traversed at max speed.
      **/
-    private List<Object> getBlocksInFront(int lane, int block) {
+    private List<Object> getBlocksInFront(int lane, int block, int speed) {
         List<Lane[]> map = gameState.lanes;
         List<Object> blocks = new ArrayList<>();
         int startBlock = map.get(0)[0].position.block;
 
         Lane[] laneList = map.get(lane - 1);
-        for (int i = max(block - startBlock, 0); i <= block - startBlock + 15; i++) {
+        for (int i = max(block - startBlock, 0); i <= block - startBlock + speed; i++) {
             if (laneList[i] == null || laneList[i].terrain == Terrain.FINISH) {
                 break;
             }
@@ -177,9 +187,11 @@ public class Bot {
     private int Obstacles(List<Object> Lane) {
         int count = 0;
         for (int i = 0; i < Lane.size(); i++) {
-            if (Lane.get(i).equals(Terrain.MUD) || Lane.get(i).equals(Terrain.WALL) ||
+            if (Lane.get(i).equals(Terrain.MUD) ||
                     Lane.get(i).equals(Terrain.OIL_SPILL)){
                 count++;
+            } else if (Lane.get(i).equals(Terrain.WALL)){
+                count += 10;
             }
         }
         return count;
@@ -187,11 +199,11 @@ public class Bot {
     // membandingkan obstacles dari 3 lane
     // CALL KALAU DIA GA DI LANE 1 ATAU 4
     private Command compareObstacles(){
-        int Lcount = Obstacles(getBlocksInFront(myCar.position.lane - 1, myCar.position.block)
+        int Lcount = Obstacles(getBlocksInFront(myCar.position.lane - 1, myCar.position.block, myCar.speed - 1)
                 .subList(0, min(myCar.speed, trackLength - myCar.position.block + 1)));
-        int Ccount = Obstacles(getBlocksInFront(myCar.position.lane, myCar.position.block)
+        int Ccount = Obstacles(getBlocksInFront(myCar.position.lane, myCar.position.block, myCar.speed - 1)
                 .subList(0, min(myCar.speed, trackLength - myCar.position.block + 1)));
-        int Rcount = Obstacles(getBlocksInFront(myCar.position.lane + 1, myCar.position.block)
+        int Rcount = Obstacles(getBlocksInFront(myCar.position.lane + 1, myCar.position.block, myCar.speed - 1)
                 .subList(0, min(myCar.speed, trackLength - myCar.position.block + 1)));
 
         if (Ccount < Lcount && Ccount < Rcount) {
@@ -210,9 +222,9 @@ public class Bot {
     }
     // membandingkan obstacles 2 lane (kiri/kanan, flag = -1 berarti kiri, flag = +1 berarti kanan)
     private Command compareTwoLanes(int flag){
-        int Ccount = Obstacles(getBlocksInFront(myCar.position.lane, myCar.position.block)
+        int Ccount = Obstacles(getBlocksInFront(myCar.position.lane, myCar.position.block, myCar.speed)
                 .subList(0, min(myCar.speed, trackLength - myCar.position.block + 1)));
-        int Pcount = Obstacles(getBlocksInFront(myCar.position.lane + flag, myCar.position.block)
+        int Pcount = Obstacles(getBlocksInFront(myCar.position.lane + flag, myCar.position.block, myCar.speed - 1)
                 .subList(0, min(myCar.speed, trackLength - myCar.position.block + 1)));
 
         if (Pcount < Ccount){
@@ -242,5 +254,165 @@ public class Bot {
             }
         }
         return found;
+    }
+
+    private int nextSpeedState (Car targetCar) {
+        switch (targetCar.speed) {
+            case 0:
+                return 3;
+            case 3:
+            case 5:
+                return 6;
+            case 6:
+                return 8;
+            case 8:
+                return 9;
+            default:
+                return targetCar.speed;
+        }
+    }
+
+    private int prevSpeedState (Car targetCar) {
+        switch (targetCar.speed) {
+            case 9:
+                return 8;
+            case 8:
+                return 6;
+            case 6:
+                return 3;
+            case 5:
+                return 3;
+            case 3:
+                return 0;
+            default:
+                return targetCar.speed;
+        }
+    }
+
+    private int carGap(Car myCar, Car targetCar){
+        return Math.abs(myCar.position.block - targetCar.position.block);
+    }
+    private boolean isInSameLane(Car myCar, Car targetCar){
+        return myCar.position.lane == targetCar.position.lane;
+    }
+
+    private boolean isInEMPRange(Car myCar, Car opponent){
+        return Math.abs(myCar.position.block - opponent.position.block) <= 1;
+    }
+
+    private int LaneBlock (Car myCar, String direction) {
+        int flag = 0;
+        if (direction == "LEFT"){
+            flag = -1;
+        } else if (direction == "RIGHT"){
+            flag = 1;
+        } else if (direction == "CENTER"){
+            flag = 0;
+        }
+        List<Lane[]> map = gameState.lanes;
+        Lane[] laneList = map.get(myCar.position.lane - 1 + flag); // tidak dikurangi 1 soalnya dia basisnya 0 (-1) dan dia ke kanan (+1)
+        int landingPosition = myCar.position.block + myCar.speed; // harus ngecek ini basis 0 atau engga
+        if (laneList[landingPosition].terrain == Terrain.BOOST) {
+            return 1;
+        } else if (laneList[landingPosition].terrain == Terrain.LIZARD) {
+            return 2;
+        } else if (laneList[landingPosition].terrain == Terrain.TWEET) {
+            return 3;
+        } else if (laneList[landingPosition].terrain == Terrain.OIL_POWER) {
+            return 4;
+        } else if (laneList[landingPosition].terrain == Terrain.EMP) {
+            return 5;
+        } else {
+            return 9;
+        }
+    }
+
+    private int accelerateLaneBlock (Car myCar) {
+        List<Lane[]> map = gameState.lanes;
+        Lane[] laneList = map.get(myCar.position.lane - 1); // dikurangi 1 soalnya dia basisnya 0
+        int landingPosition = myCar.position.block + nextSpeedState(myCar); // harus ngecek ini basis 0 atau engga
+        if (laneList[landingPosition].terrain == Terrain.BOOST) {
+            return 1;
+        } else if (laneList[landingPosition].terrain == Terrain.LIZARD) {
+            return 2;
+        } else if (laneList[landingPosition].terrain == Terrain.TWEET) {
+            return 3;
+        } else if (laneList[landingPosition].terrain == Terrain.OIL_POWER) {
+            return 4;
+        } else if (laneList[landingPosition].terrain == Terrain.EMP) {
+            return 5;
+        } else {
+            return 9;
+        }
+    }
+
+    private boolean canGoLeft (Car myCar) {
+        return (myCar.position.lane > 1);
+    }
+
+    private boolean canGoRight (Car myCar) {
+        return (myCar.position.lane < 4);
+    }
+
+    private int min3(int a, int b, int c) {
+        return (min(a, min(b, c)));
+    }
+
+    private int compareLanes(Car myCar, List<Object> left, List<Object> curr, List<Object> right){
+        boolean LPos = false; boolean RPos = false;
+        int lCount = 100, rCount = 100, cCount = 100; //asumsi isinya wall semua
+
+        if (canGoLeft(myCar)) {
+            lCount = Obstacles(left);
+            LPos = true;
+        }
+        if (canGoRight(myCar)){
+            rCount = Obstacles(right);
+        }
+
+        cCount = Obstacles(curr);
+
+        if (LPos && RPos) {
+            if (min3(lCount, rCount, cCount) == lCount){
+                if (lCount == cCount){
+                    if (lCount == rCount){
+                        return 15;
+                    } else {
+                        return 5;
+                    }
+                } else {
+                    return -1;
+                }
+            } else if (min3(lCount, rCount, cCount) == cCount){
+                if (cCount == rCount){
+                    return 10;
+                } else {
+                    return 0;
+                }
+            } else {
+                return 1;
+            }
+        } else if (LPos && !RPos){
+            if (min(lCount, cCount) == lCount){
+                if (lCount == cCount){
+                    return 5;
+                } else {
+                    return -1;
+                }
+            } else {
+                return 0;
+            }
+        } else if (!LPos && RPos){
+            if (min(rCount, cCount) == rCount){
+                if (rCount == cCount){
+                    return 10;
+                } else {
+                    return 1;
+                }
+            } else {
+                return 0;
+            }
+        }
+        return 0;
     }
 }
